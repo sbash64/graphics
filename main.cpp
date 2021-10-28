dd182dc7b2968699dc0c41cbf0a90645f2b23598
Seth Bashford <seth.bashford@protonmail.com>
2021-10-28 14:12:29 -0500

    add vulkan

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vulkan/vulkan_core.h>
+
 #include <cstdlib>
 #include <exception>
 #include <iostream>
+#include <stdexcept>
 
 namespace gflw_wrappers {
 struct Init {
@@ -11,7 +14,48 @@ struct Init {
 
   ~Init() { glfwTerminate(); }
 };
+
+struct Window {
+  Window(int width, int height)
+      : window{glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr)} {}
+
+  ~Window() { glfwDestroyWindow(window); }
+
+  GLFWwindow *window;
+};
 } // namespace gflw_wrappers
+
+namespace vulkan_wrappers {
+struct Instance {
+  Instance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames =
+        glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledLayerCount = 0;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+      throw std::runtime_error("failed to create instance!");
+  }
+
+  ~Instance() { vkDestroyInstance(instance, nullptr); }
+
+  VkInstance instance{};
+};
+} // namespace vulkan_wrappers
 
 constexpr auto windowWidth = 800;
 constexpr auto windowHeight = 600;
@@ -21,13 +65,13 @@ static void run() {
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  gflw_wrappers::Window window{windowWidth, windowHeight};
 
-  auto *window =
-      glfwCreateWindow(windowWidth, windowHeight, "Vulkan", nullptr, nullptr);
-  while (glfwWindowShouldClose(window) == 0) {
+  vulkan_wrappers::Instance vulkanInstance;
+
+  while (glfwWindowShouldClose(window.window) == 0) {
     glfwPollEvents();
   }
-  glfwDestroyWindow(window);
 }
 
 int main() {
