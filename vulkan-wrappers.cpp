2338188252d903596919e5826e8d598dc9df4ac7
Seth Bashford <seth.bashford@protonmail.com>
2021-11-10 10:51:51 -0600

    fix

--- vulkan-wrappers.cpp
+++ vulkan-wrappers.cpp
@@ -213,7 +213,7 @@ ImageView::ImageView(VkDevice device, VkImage image, VkFormat format,
   createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
   createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
   createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-  createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+  createInfo.subresourceRange.aspectMask = aspectFlags;
   createInfo.subresourceRange.baseMipLevel = 0;
   createInfo.subresourceRange.levelCount = 1;
   createInfo.subresourceRange.baseArrayLayer = 0;
