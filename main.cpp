ec2282ba2ff46646e2855ca3d0f7f1710be5f97c
Seth Bashford <seth.bashford@protonmail.com>
2021-11-11 10:45:30 -0600

    CRAZY

--- main.cpp
+++ main.cpp
@@ -653,7 +653,7 @@ static void run(const std::string &vertexShaderCodePath,
                     attrib.vertices[3 * index.vertex_index + 2]};
 
       vertex.texCoord = {attrib.texcoords[2 * index.texcoord_index + 0],
-                         attrib.texcoords[2 * index.texcoord_index + 1]};
+                         1.0f - attrib.texcoords[2 * index.texcoord_index + 1]};
 
       vertex.color = {1.0f, 1.0f, 1.0f};
 
