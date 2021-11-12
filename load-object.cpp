2ae9e68f538154551395482433a7b7e61492af78
Seth Bashford <seth.bashford@protonmail.com>
2021-11-12 09:34:47 -0600

    updated tiny obj api

--- load-object.cpp
+++ load-object.cpp
@@ -7,22 +7,18 @@
 
 namespace sbash64::graphics {
 auto readIndexedVertices(const std::string &path) -> IndexedVertices {
-  std::vector<tinyobj::shape_t> shapes;
-  tinyobj::attrib_t attrib;
-  {
-    std::vector<tinyobj::material_t> materials;
-    std::string warn;
-    std::string err;
-    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err,
-                          path.c_str()))
-      throw std::runtime_error(warn + err);
-  }
+  tinyobj::ObjReader reader;
+  tinyobj::ObjReaderConfig reader_config;
+
+  if (!reader.ParseFromFile(path, reader_config))
+    throw std::runtime_error{reader.Error()};
 
   IndexedVertices indexedVertices;
   std::unordered_map<Vertex, uint32_t> uniqueVertices{};
-  for (const auto &shape : shapes)
+  for (const auto &shape : reader.GetShapes())
     for (const auto &index : shape.mesh.indices) {
       Vertex vertex{};
+      const auto &attrib{reader.GetAttrib()};
       vertex.pos = {attrib.vertices[3 * index.vertex_index + 0],
                     attrib.vertices[3 * index.vertex_index + 1],
                     attrib.vertices[3 * index.vertex_index + 2]};
